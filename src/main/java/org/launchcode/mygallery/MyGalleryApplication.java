426242c7f142bf04ace57ead9907a31105cd3fbf
Kevin Buss <zyggy333@gmail.com>
2020-02-21 09:18:12 -0600

    Setup intial Spring Project in IntelligJ

---
+++ src/main/java/org/launchcode/mygallery/MyGalleryApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.mygallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/mygallery/MyGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.mygallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

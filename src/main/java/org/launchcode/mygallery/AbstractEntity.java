982d4f9e28fc6f1c52f6f05b1c187ee82831a31c
Kevin Buss <zyggy333@gmail.com>
2020-02-25 10:12:08 -0600

    Added GeneralUser and AbstractEntity classes

--- src/main/java/org/launchcode/mygallery/AbstractEntity.java
+++ src/main/java/org/launchcode/mygallery/AbstractEntity.java
@@ -1,4 +1,29 @@
 package org.launchcode.mygallery;
 
-public class AbstractEntity {
+import java.util.Objects;
+
+//Written by Kevin Buss
+
+public abstract class AbstractEntity {
+
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity entity = (AbstractEntity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
--- src/main/java/org/launchcode/mygallery/GeneralUser.java
+++ src/main/java/org/launchcode/mygallery/GeneralUser.java
@@ -1,4 +1,44 @@
 package org.launchcode.mygallery;
 
-public class GeneralUser {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.validation.constraints.NotNull;
+
+
+//Written by Kevin Buss
+
+
+public class GeneralUser extends AbstractEntity {
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    @NotNull
+    private String role;
+
+    public GeneralUser() {}
+
+    public GeneralUser(String username, String password, String role) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
 }

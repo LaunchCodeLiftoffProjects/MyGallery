2c606593a1ec0cef9ea9777f0b7def6e0363f4d1
JBuck31 <jrbuck31@gmail.com>
2020-02-25 19:38:04 -0600

    connected to MySQL database

---
+++ src/main/java/org/launchcode/mygallery/data/UserRegistrationRepository.java
@@ -0,0 +1,5 @@
+package org.launchcode.mygallery.data;
+
+//written by Jen Buck
+public interface UserRegistrationRepository extends CrudRepository<User, Integer> { //Get whatever the name for User class to put here
+}

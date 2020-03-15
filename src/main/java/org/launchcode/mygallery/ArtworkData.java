7cfbde851c1e7d65c4eeea00b72b6b41b073df6e
jenbuck <jrbuck31@gmail.com>
2020-03-15 11:51:45 -0500

    Created an ArtworkData class and began figuring out code for searching for artwork

---
+++ src/main/java/org/launchcode/mygallery/ArtworkData.java
@@ -0,0 +1,27 @@
+package org.launchcode.mygallery;
+//written by Jen Buck
+
+//code for searching the Artwork data by search term.
+
+import java.util.ArrayList;
+
+public class ArtworkData {
+
+    public static ArrayList<Artwork> findByValue(String value, Iterable<Artwork> artworks) {
+
+        ArrayList<Artwork> artworkResults = new ArrayList<>();// makes a list to hold the search results
+
+        for (Artwork artwork : artworks) { //iterates through each artwork
+            String aValue = getFieldValue(artwork); //this will call the method I'm creating below...
+
+            if(aValue != null && aValue.toLowerCase().contains(value.toLowerCase())){
+                artworkResults.add(artwork);
+        }
+    }
+}
+
+    private static String getFieldValue(Artwork artwork String ) {
+        String artValue;
+        if ()
+    }
+    }
--- src/main/java/org/launchcode/mygallery/controllers/SearchController.java
+++ src/main/java/org/launchcode/mygallery/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.mygallery.controllers;
 
+import org.launchcode.mygallery.Artwork;
 import org.launchcode.mygallery.data.ArtworkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 //Written by Jen Buck//
 @Controller
@@ -13,7 +19,14 @@ public class SearchController {
     private ArtworkRepository artworkRepository;
 
     @RequestMapping("")
-    public String search() {
+    public String search(Model model) {
+        model.addAttribute("artworks", artworkRepository.findAll());
+        return "searchArt";
+    }
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm){
+//put code here after making the ArtworkData class
         return "searchArt";
     }
 }

962e51e181cac6140ddf04bffc2c3438bf82c95a
jenbuck <jrbuck31@gmail.com>
2020-03-15 09:58:15 -0500

    set up GetMapping for searchArt.html

--- src/main/java/org/launchcode/mygallery/controllers/SearchController.java
+++ src/main/java/org/launchcode/mygallery/controllers/SearchController.java
@@ -1,4 +1,19 @@
 package org.launchcode.mygallery.controllers;
 
+import org.launchcode.mygallery.data.ArtworkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//Written by Jen Buck//
+@Controller
+@RequestMapping("searchArt")
 public class SearchController {
+    @Autowired
+    private ArtworkRepository artworkRepository;
+
+    @RequestMapping("")
+    public String search() {
+        return "searchArt";
+    }
 }

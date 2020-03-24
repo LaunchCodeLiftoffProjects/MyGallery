d7eb61419723d21576051fb37f8691d813dac1b5
JBuck31 <jrbuck31@gmail.com>
2020-03-24 09:25:41 -0500

    commented out some code on the search controller because it isn't completed yet, but I wanted to run the page

--- src/main/java/org/launchcode/mygallery/ArtworkData.java
+++ src/main/java/org/launchcode/mygallery/ArtworkData.java
@@ -7,21 +7,21 @@ import java.util.ArrayList;
 
 public class ArtworkData {
 
-    public static ArrayList<Artwork> findByValue(String value, Iterable<Artwork> artworks) {
+//    public static ArrayList<Artwork> findByValue(String value, Iterable<Artwork> artworks) {
+//
+//        ArrayList<Artwork> artworkResults = new ArrayList<>();// makes a list to hold the search results
+//
+//        for (Artwork artwork : artworks) { //iterates through each artwork
+//            String aValue = getFieldValue(artwork); //this will call the method I'm creating below...
+//
+//            if(aValue != null && aValue.toLowerCase().contains(value.toLowerCase())){
+//                artworkResults.add(artwork);
+//        }
+//    }
+//}
 
-        ArrayList<Artwork> artworkResults = new ArrayList<>();// makes a list to hold the search results
-
-        for (Artwork artwork : artworks) { //iterates through each artwork
-            String aValue = getFieldValue(artwork); //this will call the method I'm creating below...
-
-            if(aValue != null && aValue.toLowerCase().contains(value.toLowerCase())){
-                artworkResults.add(artwork);
-        }
-    }
-}
-
-    private static String getFieldValue(Artwork artwork String ) {
-        String artValue;
-        if ()
-    }
+//    private static String getFieldValue(Artwork artwork, String ) {
+//        String artValue;
+//        if ()
+//    }
     }
